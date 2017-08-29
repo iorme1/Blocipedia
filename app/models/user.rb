ac5b0a6b6ccdf6241bb2534ec4a563bd5d40f93f
Isaac Orme <isorme1@gmail.com>
2017-08-29 15:53:12 -0700

    user-roles: completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
+  after_initialize { self.role = :standard }
 
+  enum role: [:standard, :premium, :admin]
   has_many :wikis
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
