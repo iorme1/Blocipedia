9b02f4dc370afc94502e985e15a638d47e2a4179
Isaac Orme <isorme1@gmail.com>
2017-08-29 12:02:49 -0700

    Wiki Crud: completed

---
+++ app/controllers/wikis_controller.rb
@@ -0,0 +1,53 @@
+class WikisController < ApplicationController
+  before_action :find_wiki, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @wikis = Wiki.all
+  end
+
+  def show
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new(wiki_params)
+
+    if @wiki.save
+      redirect_to wikis_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @wiki.update(wiki_params)
+      redirect_to wiki_path(@wiki)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @wiki.destroy
+    redirect_to wikis_path
+  end
+
+  private
+
+  def wiki_params
+    params.require(:wiki).permit(:title, :body, :private)
+  end
+
+  def find_wiki
+    @wiki = Wiki.find(params[:id])
+  end
+
+
+
+end
