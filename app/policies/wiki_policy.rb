823fa9501fb3e9db577290895a1896d925bfed59
Isaac Orme <isorme1@gmail.com>
2018-10-06 17:39:29 -0700

    Refactors wiki policy

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -2,7 +2,7 @@ class WikiPolicy < ApplicationPolicy
   attr_reader :wiki, :user
 
   def initialize(user, wiki)
-    @user = user 
+    @user = user
     @wiki = wiki
   end
 
@@ -14,32 +14,29 @@ class WikiPolicy < ApplicationPolicy
   class Scope
     attr_reader :user, :scope
 
-   def initialize(user, scope)
-     @user = user
-     @scope = scope
-   end
+     def initialize(user, scope)
+       @user = user
+       @scope = scope
+     end
 
-   def resolve
-     wikis = []
-      if user.role == 'admin'
-        wikis = scope.all # if the user is an admin, show them all the wikis
-      elsif user.role == 'premium'
-        all_wikis = scope.all
-        all_wikis.each do |wiki|
-          if (wiki.private? == false) || wiki.user_id == user.id || wiki.collaborating_users.include?(user)
-            wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
-          end
+    def resolve
+      wikis = []
+
+      if user
+        if user.role == 'admin'
+          wikis = scope.all
+        elsif user.role == 'premium'
+          wikis = scope.all.select { |wiki| !wiki.private? ||
+            wiki.user_id == user.id ||
+            wiki.collaborating_users.include?(user) }
+        else  #stadard user
+          wikis = scope.all.select { |wiki| !wiki.private? ||
+            wiki.collaborating_users.include?(user) }
         end
-      else # this is the lowly standard user
-        all_wikis = scope.all
-        wikis = []
-        all_wikis.each do |wiki|
-          if (wiki.private? == false) ||  wiki.collaborating_users.include?(user)
-            wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
-          end
-        end
-       end
-      wikis # return the wikis array we've built up
+      else #guest user
+        wikis = scope.all.select { |wiki| !wiki.private? }
+      end
+      wikis
     end
   end
 end
